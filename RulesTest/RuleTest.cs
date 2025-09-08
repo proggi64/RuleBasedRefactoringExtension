7b7216ba2369b97d79b0da21861be30e11b89f30
Karsten Beck <kb.github@webkasi.de>
2025-09-08 15:04:54 +0200

    Korrekturen und erweiterte Unittests für die RegEx-Rules

--- RulesTest/RuleTest.cs
+++ RulesTest/RuleTest.cs
@@ -167,6 +167,7 @@ public sealed class RuleTest
     [DataRow("delete ptr;", "ptr = null;")]
     [DataRow("delete ptr; // Test", "ptr = null; // Test")]
     [DataRow("\tdelete ptr; // Test", "\tptr = null; // Test")]
+    [DataRow("\tx = 10;\r\n\tdelete ptr; // Test", "\tx = 10;\r\n\tptr = null; // Test")]
     public void ChangeDelete(string toChange, string expected)
     {
         string result = Apply(toChange);
@@ -179,6 +180,7 @@ public sealed class RuleTest
     [DataRow("int* ptr;", "int? ptr;")]
     [DataRow("CString* ptr;", "CString? ptr;")]
     [DataRow("void Test(CString* ptr) // test", "void Test(CString? ptr) // test")]
+    [DataRow("\ta = 10;\r\nvoid Test(CString* ptr) // test", "\ta = 10;\r\nvoid Test(CString? ptr) // test")]
     public void ChangePointerDecl(string toChange, string expected)
     {
         string result = Apply(toChange);
@@ -190,7 +192,65 @@ public sealed class RuleTest
     [TestMethod]
     [DataRow("int xy[12];", "int[] xy = new int[12];")]
     [DataRow("\tint xy [12]; // test", "\tint[] xy = new int[12]; // test")]
+    [DataRow("\ta = 10;\r\n\tint xy [12]; // test", "\ta = 10;\r\n\tint[] xy = new int[12]; // test")]
+    [DataRow("\ta = 10;\r\n\tint xy [12]; // test\r\n\ta = 10;\r\n", "\ta = 10;\r\n\tint[] xy = new int[12]; // test\r\n\ta = 10;\r\n")]
     public void ChangeArrayDecl(string toChange, string expected)
+    {
+        string result = Apply(toChange);
+        Assert.AreEqual(expected, result);
+    }
+    #endregion
+
+    #region ChangeStrCmp
+    [TestMethod]
+    [DataRow("s = strcmp(a, b);", "s = Str.strcmp(a, b);")]
+    [DataRow("\ta = 10;\r\n\ts = strcmp(a, b);\r\n\ta = 10;\r\n", "\ta = 10;\r\n\ts = Str.strcmp(a, b);\r\n\ta = 10;\r\n")]
+    [DataRow("s = Str.strcmp(a, b);", "s = Str.strcmp(a, b);")]
+    public void ChangeStrCmp(string toChange, string expected)
+    {
+        string result = Apply(toChange);
+        Assert.AreEqual(expected, result);
+    }
+    #endregion
+
+    #region ChangeTcsCmp
+    [TestMethod]
+    [DataRow("s = _tcscmp(a, b);", "s = Str.strcmp(a, b);")]
+    public void ChangeTcsCmp(string toChange, string expected)
+    {
+        string result = Apply(toChange);
+        Assert.AreEqual(expected, result);
+    }
+    #endregion
+
+    #region ChangeStriCmp
+    [TestMethod]
+    [DataRow("s = stricmp(a, b);", "s = Str.stricmp(a, b);")]
+    [DataRow("\r\nx = 10;\ts = stricmp(a, b);", "\r\nx = 10;\ts = Str.stricmp(a, b);")]
+    [DataRow("s = Str.stricmp(a, b);", "s = Str.stricmp(a, b);")]
+    public void ChangeStriCmp(string toChange, string expected)
+    {
+        string result = Apply(toChange);
+        Assert.AreEqual(expected, result);
+    }
+    #endregion
+
+    #region ChangeTcsCmpi
+    [TestMethod]
+    [DataRow("s = _tcscmpi(a, b);", "s = Str.stricmp(a, b);")]
+    public void ChangeTcsCmpi(string toChange, string expected)
+    {
+        string result = Apply(toChange);
+        Assert.AreEqual(expected, result);
+    }
+    #endregion
+
+    #region ChangeStrnCmp
+    [TestMethod]
+    [DataRow("s = strncmp(a, x, b);", "s = Str.strncmp(a, x, b);")]
+    [DataRow("\ta = 10;\r\n\ts = strncmp(a, x, b);\r\n\ta = 10;\r\n", "\ta = 10;\r\n\ts = Str.strncmp(a, x, b);\r\n\ta = 10;\r\n")]
+    [DataRow("s = Str.strncmp(a, xyz, b);", "s = Str.strncmp(a, xyz, b);")]
+    public void ChangeStrnCmp(string toChange, string expected)
     {
         string result = Apply(toChange);
         Assert.AreEqual(expected, result);
